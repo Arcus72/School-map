151899b9972c5ca1996fc0651bd1084f31923313
Arcus72 <kimla.20034@gmail.com>
2022-05-23 21:43:48 +0200

    Added signature and animation for inputs

--- src/components/MapForm/MapForm.jsx
+++ src/components/MapForm/MapForm.jsx
@@ -43,29 +43,42 @@ function MapForm({
       }`}
     >
       <div className='Form'>
-        <input
-          className='Form__input'
-          type='text'
-          placeholder='Gdzie jesteś ?'
-          list='pointNameInput'
-          ref={startPointInput}
-        />
+        <div className='Form__inputField'>
+          <input
+            className='Form__input'
+            id='Form__from'
+            type='text'
+            placeholder='Gdzie jesteś?'
+            list='pointNameInput'
+            ref={startPointInput}
+          />
+          <label className='Form__inputLabel' htmlFor='Form__from'>
+            Gdzie jesteś?
+          </label>
+        </div>
         <br />
-        <input
-          className='Form__input'
-          type='text'
-          placeholder='Dokąd zmierzasz ?'
-          list='pointNameInput'
-          ref={endPointInput}
-        />
+        <div className='Form__inputField'>
+          <input
+            className='Form__input'
+            id='Form__to'
+            type='text'
+            placeholder='Dokąd zmierzasz?'
+            list='pointNameInput'
+            ref={endPointInput}
+          />
+          <label className='Form__inputLabel' htmlFor='Form__to'>
+            Dokąd zmierzasz?
+          </label>
+        </div>
         <datalist id='pointNameInput'>
           {spreadRoomsLocationToArray().map((text) => (
             <option key={text} value={text} />
           ))}
         </datalist>
         <br />
-        <label className='Form__label'>Jakosc :</label>
         <div className='Form__section'>
+          <h4 className='Form__qualityHeader'>Jakość:</h4>
+          image.png{' '}
           <button
             onClick={ToggleMapQuality}
             className={`Form__btnInForm Form__qualityBtn ${
@@ -81,7 +94,7 @@ function MapForm({
               !mapQuality && 'Form__qualityBtn--active'
             }`}
           >
-            Wysoko
+            Wysoka
           </button>
         </div>
         <br />
@@ -89,12 +102,16 @@ function MapForm({
           className='Form__showBtn Form__btnInForm Form__showBtn'
           onClick={() => validateForm()}
         >
-          Pokaz
+          Pokaż
           <span className='button__span'></span>
           <span className='button__span'></span>
           <span className='button__span'></span>
           <span className='button__span'></span>
         </button>
+      </div>
+      <div className='Form__signature'>
+        Maciej Kimla 3K-G <br />
+        Kamil Kozik 3K-G
       </div>
     </div>
   );
